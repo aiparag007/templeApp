b2532b44a2e45a017504c1a77b2ae28a06d53bde
Parag Jain <85475704+aiparag007@users.noreply.github.com>
2025-03-15 21:05:53 +0530

    Update temple.py

--- temple.py
+++ temple.py
@@ -2,8 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 # import streamlit_authenticator as stauth
-import yaml
-from yaml.loader import SafeLoader
+# import yaml
+# from yaml.loader import SafeLoader
 from streamlit_phone_number import st_phone_number
 import pgeocode
 import phonenumbers
